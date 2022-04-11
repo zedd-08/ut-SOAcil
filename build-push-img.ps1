a55566d620b7547729ebbf55ccf2c9ca0520f910
zedd-08 <manish.96@live.com>
2022-04-11 21:10:21 +0200

    Added gradle clean

--- build-push-img.ps1
+++ build-push-img.ps1
@@ -1,23 +1,27 @@
 Set-Location 'Authentication-Service'
 # .\gradlew.bat bootBuildImage --imageName=zedd08/soacil-auth-service
+.\gradlew.bat clean
 .\gradlew.bat bootJar
 docker build . -t zedd08/soacil-auth-service
 docker push zedd08/soacil-auth-service
 
 Set-Location '..\Notification-Service'
 # .\gradlew.bat bootBuildImage --imageName=zedd08/soacil-notif-service
+.\gradlew.bat clean
 .\gradlew.bat bootJar
 docker build . -t zedd08/soacil-notif-service
 docker push zedd08/soacil-notif-service
 
 Set-Location '..\Post-Service'
 # .\gradlew.bat bootBuildImage --imageName=zedd08/soacil-post-service
+.\gradlew.bat clean
 .\gradlew.bat bootJar
 docker build . -t zedd08/soacil-post-service
 docker push zedd08/soacil-post-service
 
 Set-Location '..\User-Service'
 # .\gradlew.bat bootBuildImage --imageName=zedd08/soacil-user-service
+.\gradlew.bat clean
 .\gradlew.bat bootJar
 docker build . -t zedd08/soacil-user-service
 docker push zedd08/soacil-user-service
--- build-push-img.sh
+++ build-push-img.sh
@@ -1,23 +1,27 @@
 cd Authentication-Service
 # ./gradlew bootBuildImage --imageName=zedd08/soacil-auth-service
+./gradlew clean
 ./gradlew bootJar
 docker build . -t zedd08/soacil-auth-service
 docker push zedd08/soacil-auth-service
 
 cd ../Notification-Service
 # ./gradlew bootBuildImage --imageName=zedd08/soacil-notif-service
+./gradlew clean
 ./gradlew bootJar
 docker build . -t zedd08/soacil-notif-service
 docker push zedd08/soacil-notif-service
 
 cd ../Post-Service
 # ./gradlew bootBuildImage --imageName=zedd08/soacil-post-service
+./gradlew clean
 ./gradlew bootJar
 docker build . -t zedd08/soacil-post-service
 docker push zedd08/soacil-post-service
 
 cd ../User-Service
 # ./gradlew bootBuildImage --imageName=zedd08/soacil-user-service
+./gradlew clean
 ./gradlew bootJar
 docker build . -t zedd08/soacil-user-service
 docker push zedd08/soacil-user-service
